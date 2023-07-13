9b3b850e71593ed944a60113b9cd27c712aec19b
Mainfrezzer <35963833+Mainfrezzer@users.noreply.github.com>
2023-07-13 10:12:43 +0200

    update to b3103643-ls135

--- root/app/duck.sh
+++ root/app/duck.sh
@@ -10,6 +10,30 @@ else
     DUCK_LOG="/dev/null"
 fi
 
+if [ "${IPV6}" = "dual" ];
+then
+	{
+	ipv4addr=$(curl -s -4 "https://ifconfig.io")
+	ipv6addr=$(curl -s -6 "https://ifconfig.io")
+	RESPONSE=$(curl -sS --max-time 60 "https://www.duckdns.org/update?domains=${SUBDOMAINS}&token=${TOKEN}&ip=${ipv4addr}&ipv6=${ipv6addr}")
+	if [ "${RESPONSE}" = "OK" ]; then
+		echo "Your IP was updated at $(date)"
+	else
+		echo -e "Something went wrong, please check your settings $(date)\nThe response returned was:\n${RESPONSE}"
+	fi
+	} | tee -a "${DUCK_LOG}"
+elif [ "${IPV6}" = "only" ];
+then
+	{
+	ipv6addr=$(curl -s -6 "https://ifconfig.io")
+	RESPONSE=$(curl -sS --max-time 60 "https://www.duckdns.org/update?domains=${SUBDOMAINS}&token=${TOKEN}&ipv6=${ipv6addr}")
+	if [ "${RESPONSE}" = "OK" ]; then
+		echo "Your IP was updated at $(date)"
+	else
+		echo -e "Something went wrong, please check your settings $(date)\nThe response returned was:\n${RESPONSE}"
+	fi
+	} | tee -a "${DUCK_LOG}"
+else
 {
     RESPONSE=$(curl -sS --max-time 60 "https://www.duckdns.org/update?domains=${SUBDOMAINS}&token=${TOKEN}&ip=")
     if [ "${RESPONSE}" = "OK" ]; then
@@ -18,3 +42,4 @@ fi
         echo -e "Something went wrong, please check your settings $(date)\nThe response returned was:\n${RESPONSE}"
     fi
 } | tee -a "${DUCK_LOG}"
+fi
